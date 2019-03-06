176bd7cb8c3f4e9f1559465332d702918f2f4b1e
satishf889 <36239702+satishf889@users.noreply.github.com>
2019-03-06 16:31:41 +0530

    Latest Model With User Interface

---
+++ admisson.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+	return render_template('home1.html')
+
+@app.route('/front')
+def my_form_return():
+	return render_template('home1.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    gre = int(request.form['GRE'])
+    toefl=int(request.form['TOEFL'])
+    ur=int(request.form['UR'])
+    sop=float(request.form['SOP'])
+    lor=float(request.form['LOR'])
+    cgpa=float(request.form['CGPA'])
+    r=int(request.form['R'])
+    #processed_text = text.upper()
+    print(request.form)
+    loaded_model = pickle.load(open('admission_model.pkl', 'rb'))
+    result=loaded_model.predict([[gre,toefl,ur,sop,lor,cgpa,r]])
+    expected_per=round((result[0]*100),2)
+    answer="The chance of getting admission after graduation is "+str(expected_per)+"%"
+    return render_template('result.html',answer=answer)
+
+if __name__ == "__main__":
+    app.run()
