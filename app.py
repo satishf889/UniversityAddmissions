142461014dcdbe0eb6b77fcb1e3aad6d30994be0
satishf889 <36239702+satishf889@users.noreply.github.com>
2019-04-04 13:05:17 +0530

    Heroku Changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import os
 import numpy as np
 import pickle
 
@@ -30,4 +31,5 @@ def my_form_post():
     return render_template('result.html',answer=answer)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
